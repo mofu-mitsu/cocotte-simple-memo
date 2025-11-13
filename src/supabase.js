2d8d5d532ec409175f0d57e46c8acee7a5de6cff
もふみつ <mitsuki.momoka@i.softbank.jp>
2025-11-13 20:34:22 +0900

    Update Supabase client configuration to use environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+// supabase.js
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yeveuzdezowqijcyyxiv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlldmV1emRlem93cWlqY3l5eGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4Njc0NjAsImV4cCI6MjA3NDQ0MzQ2MH0.5Kd9ZNfLR0Hzpnz1H1tBXzvP_CWUD0u00c2UsfD1GfI';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
